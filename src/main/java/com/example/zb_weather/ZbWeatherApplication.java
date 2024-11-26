c76aff5702ff723b2c63b67fa5b65edefa093626
LeeSooBin <romaydev@naver.com>
2024-11-26 12:47:42 +0900

    first commit

---
+++ src/main/java/com/example/zb_weather/ZbWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.zb_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zb_weather/ZbWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zb_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
